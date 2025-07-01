2ddf25c0eb33bb984a79fba94f42c0425a1e4981
wieslekbaranowski <wieslaw.baranowski@intive.com>
2025-07-01 03:54:58 +0200

    version 0.0.0

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,20 @@ fn main() {
     let order_data = pages::get_order_pages(&args, order_old_all_data);
     println!("order_data.len: {:?}", order_data.len());
 
+    let mut order_book = new_file_empty_worksheet();
+    let mut all_order_book = new_file_empty_worksheet();
+    order_book = process(order_book, &order_data, IT_ORDERS, &args, false);
+    all_order_book = process(all_order_book, &order_data, ORDERS, &args, true);
+    let _ = writer::xlsx::write(&order_book, std::path::Path::new(IT_ORDERS_FILENAME));
+    let _ = writer::xlsx::write(&all_order_book, std::path::Path::new(ORDERS_FILENAME));
+
     let tender_data = pages::get_tender_pages(&args, tender_old_all_data);
     println!("tender_data.len: {:?}", tender_data.len());
 
     let mut tender_book = new_file_empty_worksheet();
-    let mut offer_book = new_file_empty_worksheet();
     let mut all_tender_book = new_file_empty_worksheet();
-    let mut all_offer_book = new_file_empty_worksheet();
     tender_book = process(tender_book, &tender_data, IT_TENDERS, &args, false);
     all_tender_book = process(all_tender_book, &tender_data, TENDERS, &args, true);
-    offer_book = process(offer_book, &order_data, IT_ORDERS, &args, false);
-    all_offer_book = process(all_offer_book, &order_data, ORDERS, &args, true);
     let _ = writer::xlsx::write(&tender_book, std::path::Path::new(IT_TENDERS_FILENAME));
-    let _ = writer::xlsx::write(&offer_book, std::path::Path::new(IT_ORDERS_FILENAME));
     let _ = writer::xlsx::write(&all_tender_book, std::path::Path::new(TENDERS_FILENAME));
-    let _ = writer::xlsx::write(&all_offer_book, std::path::Path::new(ORDERS_FILENAME));
 }
